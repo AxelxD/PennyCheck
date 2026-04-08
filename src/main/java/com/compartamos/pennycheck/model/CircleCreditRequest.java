03bf132c7d4884b545eee07ec77b7e6642a282ec
Miguel Pineda <rusheraxel@gmail.com>
2026-04-07 21:51:05 -0600

    Modelos

---
+++ src/main/java/com/compartamos/pennycheck/model/CircleCreditRequest.java
@@ -0,0 +1,9 @@
+package com.compartamos.pennycheck.model;
+
+public class CircleCreditRequest {
+    public String account_number;
+    public String bank_code;
+    public String account_type;
+    public String customer_id;
+    public String request_timestamp;
+}
---
+++ src/main/java/com/compartamos/pennycheck/model/CircleCreditResponse.java
@@ -0,0 +1,7 @@
+package com.compartamos.pennycheck.model;
+
+public class CircleCreditResponse {
+    public String transactionId;
+    public String status;
+    public String message;
+}
